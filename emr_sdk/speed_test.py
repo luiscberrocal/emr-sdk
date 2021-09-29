ada42d1e2955ec2714509354684a86d74b5279eb
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-09-28 21:39:31 -0500

    Catching errors

--- emr_sdk/speed_test.py
+++ emr_sdk/speed_test.py
@@ -9,22 +9,29 @@ import speedtest
 def write_header(output_filename):
     with open(output_filename, 'w') as csvfile:
         spamwriter = csv.writer(csvfile)
-        spamwriter.writerow(['computer', 'ssid', 'time_stamp', 'Download MB/s', 'Upload MS/s'])
+        spamwriter.writerow(['computer', 'ssid', 'time_stamp', 'Download MB/s',
+                             'Upload MS/s', 'Comment'])
 
 
 def measure_speed(output_filename, computer, ssid, frequency):
     write_header(output_filename)
     for i in range(1, 11):
-        speed = speedtest.Speedtest()
-        dnld = speed.download() / 1024 / 1024
-        upld = speed.upload() / 1024 / 1024
-        print(f'{i}. Download: {dnld} MB/s')
-        print(f'     Upload  : {upld} MB/s')
-        print('-' * 50)
+        time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        try:
+            speed = speedtest.Speedtest()
+            dnld = speed.download() / 1024 / 1024
+            upld = speed.upload() / 1024 / 1024
+            comment = ''
+            print(f'{i}. Download: {dnld} MB/s')
+            print(f'     Upload  : {upld} MB/s')
+            print('-' * 50)
+        except speedtest.SpeedtestBestServerFailure:
+            dnld = 0
+            upld = 0
+            comment = 'Error'
         with open(output_filename, 'a') as csvfile:
             spamwriter = csv.writer(csvfile)
-            time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            spamwriter.writerow([computer, ssid, time_stamp, dnld, upld])
+            spamwriter.writerow([computer, ssid, time_stamp, dnld, upld, comment])
         sleep(frequency)
 
 
