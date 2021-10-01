04994142badfe44d474e4e3b1f238934deae9b4e
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-10-01 13:37:15 -0500

    Convert settings file to dictionary

--- emr_sdk/django_settings.py
+++ emr_sdk/django_settings.py
@@ -1,6 +1,7 @@
 import importlib
 from pathlib import Path
 
+import environ
 from dotenv import load_dotenv
 
 env_path = Path(__file__).parent.parent / '.env/.django'
@@ -8,15 +9,23 @@ env_path = Path(__file__).parent.parent / '.env/.django'
 load_dotenv(dotenv_path=env_path)
 
 
-def do_compare(settings):
-    fn = '/Users/luiscberrocal/PycharmProjects/emr_sdk/output/base.py'
-    spec = importlib.util.spec_from_file_location(settings, fn)
+def read_settings(settings_file, module_name='my_module'):
+    spec = importlib.util.spec_from_file_location(module_name, settings_file)
     my_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(my_module)
-
+    settings = dict()
     module_attributes = dir(my_module)
     for i, att in enumerate(module_attributes):
-        print(f'{i} {att} {type(getattr(my_module, att))}')
+        data_type = type(getattr(my_module, att))
+        record = data_type in [str, int, dict, list, set, bool, environ.environ.Path] and '__' not in att
+        rec = '[x]' if record else '[ ]'
+        print(f'{i} {att:30} {data_type}  {rec}')
+        if record:
+            settings[att] = getattr(my_module, att)
+    return settings
+
 
 if __name__ == '__main__':
-    do_compare('conf')
+    fn = '/Users/luiscberrocal/PycharmProjects/emr_sdk/output/base.py'
+    res = read_settings(fn, 'base_settings')
+    print(res)
